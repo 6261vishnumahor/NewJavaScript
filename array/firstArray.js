e7a3ff4e412d520651f6df374568a11d595a94e2
6261vishnumho <7225vishnumahor@gmail.com>
2025-07-08 16:07:23 +0530

    add files

---
+++ array/firstArray.js
@@ -0,0 +1,26 @@
+// let arr=["joy","jon",'vipin', "mohit","harsit"]
+// let arr1=["sarita","subham ,"  ]
+// let arr3=arr1+ arr
+// console.log(arr3)
+// console.log(arr.toString())
+// arr.push="bbbbbb"
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// console.log(arr.shift())
+// console.log(arr)
+// console.log(arr.unshift("vivhh"))
+// console.log(arr)
+
+
+// console.log(arr.slice([0] ,[3]))
+let arr4 = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(arr4)
+// arr4.splice(2,0,201)
+// arr4.splice(2,2,22,23)
+// arr4.splice(3,1)
+// arr4.splice(3,1,56)
+// console.log(arr4)
+let arr5 = [12, 13, 14, 15]
+let arr6 = arr4.concat(arr5)
+console.log(arr6)
---
+++ function/firstF.js
@@ -0,0 +1,43 @@
+// function sandhya(){
+//  let a=4
+//  let b=5
+// return a+b*b/a-b*(a+b+a)
+// }
+
+// console.log(sandhya())
+
+
+// const f=()=>{
+// let b=prompt("Enter the value")
+// let v=parseFloat(b,7)
+// let a=prompt("Enter the value")
+// let h=parseFloat(a,8)
+// window.alert(v+h)
+// }
+// f();
+
+
+
+
+// const g=()=>{
+//     a=5
+//     console.log((22/7)*5*5)
+// }
+// g();
+// =====================================================
+
+// Cels=(farh-32)*5/9;
+
+// const v=function(f,h){
+  
+//    let c=(f-32)*5/9
+   
+// console.log("result: "+c)
+// h()
+// }
+//  v(9,function(){
+//     console.log("the celsious :" )
+    
+// })
+// let s=k( celsious())
+// console.log(s)
