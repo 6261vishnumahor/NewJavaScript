a16ccb1894267cf4157f574f36c5b438bf496a82
6261vishnumho <7225vishnumahor@gmail.com>
2025-07-11 22:45:04 +0530

    add files

---
+++ data_type/data-type.js
@@ -0,0 +1,38 @@
+// ==================================Number data_type===============================
+// let a=234
+// console.log(a)
+// console.log(typeof(a))
+// ======================================BigInt data_type=========================================================
+// let b=23475985898n
+// console.log(typeof(b))
+
+// =============================== string data_type============================================
+// let str="a"
+// console.log(str)
+// let str2="learn the javaScript data_type"
+// console.log(str2)
+// console.log(typeof(str2))
+// console.log(str2.length)
+// let str3=str2+str
+// console.log(str3)
+// ============================================= null data_type==============================================
+
+// let a=null;   //initialise the null data_type so that a object data_type
+// console.log(typeof(a))
+
+// ================================================ undefined data_type===================================================
+
+// let c;
+// console.log( typeof(c))
+// =====================================Boolean data_type===============================
+
+// this conperision opareter
+// let a= 12>10
+// console.log(a)
+// console.log(typeof(a))
+
+// ===================================== Symbol data_type=====================================
+
+let sym=Symbol("vishnu")
+console.log(sym)
+console.log(typeof(sym))
