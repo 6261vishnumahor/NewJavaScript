fc99118be64e453f203b4be5f9028ee386236ed6
6261vishnumho <7225vishnumahor@gmail.com>
2025-07-27 20:41:52 +0530

    add

---
+++ looping/first.js
@@ -0,0 +1,11 @@
+// let a=2;
+// for(let i=1;i<=10; i++){
+//     b=a*i
+//     console.log(a,"*",i,"=",b)
+// }
+
+
+
+for(let i=1; i<=100; i++){
+console.log(i)
+}
--- looping/prime number.js
+++ looping/prime number.js
@@ -1,6 +1,24 @@
-let i=1
-for(let j=2;j<=100;j++){
-    if(i%j==0){
-        console.log(i+"is a pri")
+num=100;
+function isPrime(num) {
+    // Numbers less than or equal to 1 are not prime
+    if (num <= 1) {
+        return false;
+    }
+    // Check for divisibility from 2 up to the square root of the number
+    // If a number has a divisor greater than its square root, it also has one smaller than its square root.
+    for (let i = 2; i <= num; i++) {
+        if (num % i === 0) {
+            return false; // If divisible, it's not prime
+        }
+    }
+    return true; // If no divisors found, it's prime
+}
+
+function printPrimesUpTo(limit) {
+    console.log(`Prime numbers from 1 to ${ limit }:`);
+    for (let i = 2; i <= limit; i++) { // Start from 2 as 1 is not prime
+        if (isPrime(i)) {
+            console.log(i);
+        }
     }
 }
