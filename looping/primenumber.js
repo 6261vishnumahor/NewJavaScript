87bddaf3b6e2045f6b82c064087cbc7ed40b0cb8
6261vishnumho <7225vishnumahor@gmail.com>
2025-07-28 23:49:06 +0530

    add files

--- looping/primenumber.js
+++ looping/primenumber.js
@@ -1,6 +1,7 @@
 let count = 0
 let i, j
-for (j = 2; j <= 100; j++) {
+let all=0
+for (j = 2; j <= 200; j++) {
     for (i = 1; i <= j; i++) {
         if (j % i == 0)
             count++
@@ -8,11 +9,16 @@ for (j = 2; j <= 100; j++) {
 
     }
 
-    if (count == 2)
+    if (count == 2){
 
         console.log(j)
+    all++
+}
     count = 0 
+    // all++
+    // console.log("total prime number found so far:",all)
 
 
 
 }
+console.log("total prime number found so far:", all)
