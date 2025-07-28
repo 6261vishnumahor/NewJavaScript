d0991df131a9a5cad50fe03997d0a5ce47e31e5a
6261vishnumho <7225vishnumahor@gmail.com>
2025-07-28 22:53:58 +0530

    add files

---
+++ looping/do _while.js
@@ -0,0 +1,7 @@
+let i=1
+do{
+    console.log(i)
+    i++
+}
+while(i<=10);
+    // This loop will print numbers from 1 to 10
--- looping/first.js
+++ looping/first.js
@@ -7,5 +7,17 @@
 
 
 for(let i=1; i<=100; i++){
-console.log(i)
+    if(i%3==0){
+        console.log("fizz",i)
+    }
+     else if(i%5==0){
+        console.log("buss",i)
+     }
+     else if(i%3==0&& i%5==0){
+        console.log("fizzbuss",i)
+     }
+     else{
+console.log("not fizz or buss",i)
+     }
+
 }
