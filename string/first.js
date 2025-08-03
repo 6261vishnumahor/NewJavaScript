a36e6bef92bbffbd6bd985921d8e8e8b78c0b582
6261vishnumho <7225vishnumahor@gmail.com>
2025-08-03 21:57:33 +0530

    add files

---
+++ string/first.js
@@ -0,0 +1,95 @@
+// let str="Hello word is the a tody";
+// console.log(str)
+// console.log(str[8])
+
+
+//---length------
+// console.log(str.length)
+// console.log(str.length[0])
+
+//-----charAt----
+//charAt dose not take a (-)minus indexing
+// console.log(str.charAt(9))
+// console.log(str.charAt(-2))
+
+//------------indexOf-------------
+// console.log(str.indexOf('t'))
+// console.log(str.lastIndexOf('word'))
+// console.log(str.lastIndexOf('tody'))
+
+//-----repeat()------
+// repeat a full string in this method
+// console.log(str.repeat(3))
+// console.log(str.repeat(10))
+
+
+// ---------split------
+//split method remove the particular word letter and space
+// console.log(str.split(" "))
+// console.log(str.split("t"))
+// console.log(str.split("tody"))
+
+//----string at method----
+//find the particular letter throw the index
+// console.log(str.at(6))
+
+
+//charCodeAt------
+//find the ascii value in letter in throw the indexing
+// console.log(str.charCodeAt(8))
+
+//
+//-----------charPointAt-----------------
+
+//find the ascii value in letter in throw the indexing
+
+// console.log(str.codePointAt())
+
+
+
+//substring---------
+//remove the word in throw the index
+//not supporting a minus indexing
+//or minus index take a jeero(0) value indexing
+// console.log(str.substring(1, 5))
+// console.log(str.substring(1, -5))
+// console.log(str.substring(-5,14))
+
+
+
+//substr---------
+//not supporting a minus indexing
+// console.log(str.substr(12))
+// console.log(str.substr(5, 15))
+
+//-----concat-----
+//adding the string in this method
+//take a parameter
+let a='67';
+let b='89';
+let c=a+b
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a * b)
+// console.log(a*b)
+// console.log(a * b)
+// console.log(a.concat(c, b, a))
+
+//-----slice-----
+//run the code in  lessthen to graterthen
+
+let str = "Hello word is the a tody";
+
+// console.log(str.slice(4, 9))
+// console.log(str.slice(-9, -1))
+// console.log(str.slice(-9,12))
+// console.log(str.slice(9, -3))
+// console.log(str.slice(-1,3))
+// console.log(str.slice(4, -12))
+// console.log(str.slice(4, 1))
+
+
+
+
+
+
