ec4e696d979e9bc5db2cbbe2f15339269aed16f1
6261vishnumho <7225vishnumahor@gmail.com>
2025-08-05 00:19:23 +0530

    add files

--- looping/strongnumber.js
+++ looping/strongnumber.js
@@ -1,23 +1,54 @@
-let i = 1;
-let count = 1;
-let digit = 0;
-let sum = 0;
-let number = 145;
-let n = number;
+//first methode in strong number
+ 
+// let i = 1;
+// let count = 1;
+// let digit = 0;
+// let sum = 0;
+// let number = 145;
+// let n = number;
 
-for (let j=1; j<=n; i++){
-    digit = n % 10;
-    for (i = 1; i <= digit; i++) {
-        count = count * i;
-    }
-    sum = sum + count;
-    n = n / 10;
+// while (n!==0){
+//     digit = n % 10;
+//     for (i = 1; i <= digit; i++) {
+//         count = count * i;
+//     }
+//     sum = sum + count;
+//     count=1;
+//     n = parseInt(n / 10) ;  // n change the integer number not change a floating number
+// }
+
+// if (number == sum) {
+//     console.log("Enter number is a strong", sum)
+// }
+
+// else {
+//     console.log("Enter number is not a strong", sum)
+// }
+
+
+//secound methode in strong number
+let sum=0;
+let number=145
+let num=number
+while(num>0){
+    let x=num%10
+    let factorial= findFact(x);
+    sum=sum+factorial
+    num=parseInt(num/10)
 }
 
-if (number == sum) {
+
+function findFact(x){
+    let fact=1
+    for(let i=1; i<=x; i++){
+        fact=fact*1;
+    }
+    return fact;
+}
+if (num == sum) {
     console.log("Enter number is a strong", sum)
 }
-
 else {
     console.log("Enter number is not a strong", sum)
 }
+
